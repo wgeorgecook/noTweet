25471ebf035ff67fa8f8fe9ebf1477e651e3e1fa
William George Cook <wgeorgecook@gmail.com>
2018-10-03 20:44:06 -0700

    Function to check the date. Set this to the day you want celebrate no tweeting and tweets won't display on that date when enabled

--- noTweet.js
+++ noTweet.js
@@ -1,6 +1,15 @@
 // Set the default state of the extension button and stream
 let toolbar = "off";
+let tnd, d, today;
 
+d = new Date();
+today = `${d.getMonth()}/${d.getDate()}/${d.getFullYear()}`
+
+if (today === '6/30/2018') {
+  tnd = true;
+} else {
+  tnd = false;
+}
 
 // Function to toggle the tweet stream when invoked
 function toggle(switcher) {
@@ -28,7 +37,29 @@ function toggle(switcher) {
 
 }
 
-chrome.browserAction.onClicked.addListener(function(tab) {
+function closeTwitter(tab) {
+  if (tab.url === 'twitter.com') {
+    chrome.tabs.remove(tab);
+  }
+};
+
+chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
+  let url = tabs[0].url;
+  console.log(url);
+});
+
+// Check the date on tab update and if it's Tweet Nothing Day
+// prevent opening twitter.com
+chrome.tabs.onUpdated.addListener(function(Tab) {
+
+  if (tnd === true) {
+    console.log(Tab.url);
+    // closeTwitter(tab);
+  }
+});
+
+// Add the listener to toggle the tweet stream and button
+chrome.browserAction.onClicked.addListener(function() {
   // Invoke the toggle function when the toolbar is clicked
   toggle(toolbar);
 });
